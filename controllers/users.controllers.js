317787b47d9dcb6023f0bb15118800eeb1bd3f5f
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-05-17 12:15:05 +0100

    delete console log from controllers

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,7 +3,6 @@ const {fetchUsers } = require("./../models/users.models");
 exports.getAllUsers = (request, response) => {
     fetchUsers()
     .then((usernames) => {
-        console.log(usernames)
        response.status(200).send({usernames})
     }).catch((error) => {
        console.log(error)
