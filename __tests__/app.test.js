5047c8eaeda5dc4d43ebb20257eccebabd608000
Alicia Barczak <aliciabarczak@Alicias-MacBook-Air.local>
2022-05-18 11:14:32 +0100

    changed configuration of jest-sorted

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,6 @@ const app = require('../app');
 const db = require('../db/connection');
 const seed = require("./../db/seeds/seed");
 const testData = require("./../db/data/test-data");
-require('jest-sorted');
 
 
 beforeEach(() => {
